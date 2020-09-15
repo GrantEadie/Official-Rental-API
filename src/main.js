e3d83110553bb930ac639bcca05849f8ce062fad
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-09-15 16:19:51 -0700

    almost finished neighborhoods to neighborhood.js. Added some UI logic to handle new display elements in the main.js and index.html files.
    Co-authored-by: Forrest Reiland <freiland@email.arizona.edu>

--- src/main.js
+++ src/main.js
@@ -16,22 +16,16 @@ function getElements(response){
     $("#outputListing1Bedrooms").text("Bedrooms: " + response.listings[0].bedrooms);
     $("#outputListing1Bathrooms").text("Bathrooms: " + response.listings[0].bathrooms);
     
-
     $("#outputListing2").text(response.listings[1]);
     $("#outputListing3").text(response.listings[2]);
     $("#outputListing4").text(response.listings[3]);
     $("#outputListing5").text(response.listings[4]);
-    console.log(response.listings);
     
     // Display Listing TEST TEST//
     // lisitng array from API call will be a variable number of elements from 1-10 //
     
-    
-    
-    
-    
     response.listings.forEach(function(property) {
-      console.log(property);
+      $("rentData");
 
       console.log(property.address);
       //$("#outputListing1Address").text(property.address);
@@ -46,8 +40,6 @@ function getElements(response){
       
       //$("#outputListing1Bathrooms").text(response.listings[0].bathrooms);
     });
-
-    
     
     // use a carousel or other bootstrap feature to allow user to look through lisitngs in neighborhood //
 
@@ -62,7 +54,8 @@ $(document).ready(function () {
   $("#executeButton").click(function () {
     // test input
     let neighborhoodInput = "laurelhurst";
-
+    let displayNeighbor = neighborhoodInput.charAt(0).toUpperCase()+neighborhoodInput.slice(1);
+    console.log(displayNeighbor);
     // let neighborhoodInput = $("#neighborhood");
     // findNeighborhoodCoordinates(neighborhoodInput);
     let input = 1200;
@@ -84,17 +77,19 @@ $(document).ready(function () {
     const longitudeInput = neighborhoods[neighborhoodInput].longitude;
 
     // FORREST TEST CODE //
-
     
     console.log(neighborhoods[neighborhoodInput].latitude);
-    
-    
-    
     
     RentalService.getRentals(squareFootageInput, bathroomInput, latitudeInput, longitudeInput, propertyTypeInput, bedroomInput) 
       .then(function(response) {
         getElements(response);
-        
+        $("#rentData h3").text(`The average rent for your search criteria in ${displayNeighbor} neighborhood is $ ${response.rent}`);
+        $("#rentData p").text(`The lowest rent is $ ${response.rentRangeLow} and the high rent is $ ${response.rentRangeHigh} `);
+        $("#rentData ul").html("<li>" + response.listings[0].address + "</li>" + "<li>" + response.listings[1].address + "</li>");
+        $("#rentData ul").html("<li>" + response.listings[1].address + "</li>");
+        $("#rentData ul").html("<li>" + response.listings[2].address + "</li>");
+        $("#rentData ul").html("<li>" + response.listings[3].address + "</li>");
+        $("#rentData ul").html("<li>" + response.listings[4].address + "</li>");
       });
   });  
 });
--- src/neighborhood.js
+++ src/neighborhood.js
@@ -7,6 +7,10 @@ export let neighborhoods = {
     latitude : 45.552319,
     longitude :  -122.654043
   },
+  arborLodge: {
+    latitude : 45.571096,
+    longitude :  -122.690711
+  },
   argay: {
     latitude : 45.552285,
     longitude :  -122.520164
@@ -19,17 +23,65 @@ export let neighborhoods = {
     latitude : 45.549749,
     longitude : -122.672539
   },
+  brentwoodDarlington: {
+    latitude : 45.468331,
+    longitude : -122.598068
+  },
   bridgeton: {
     latitude : 45.602286,
     longitude : -122.668072
+  },
+  bridlemile: {
+    latitude : 45.492269,
+    longitude : -122.727550
+  },
+  brooklyn: {
+    latitude : 45.494521,
+    longitude : -122.651664
   },
   buckman: {
     latitude : 45.518169,
     longitude :  -122.648421
   },
+  cathedralPark: {
+    latitude : 45.587814,
+    longitude :  -122.757214
+  },
   centennial: {
     latitude : 45.505278,
     longitude :  -122.499159
+  },
+  center: {
+    latitude : 45.524504,
+    longitude :  -122.606995
+  },
+  concordia: {
+    latitude : 45.565746,
+    longitude :  -122.632625
+  },
+  cully: {
+    latitude : 45.561300,
+    longitude :  -122.602049
+  },
+  eastmoreland: {
+    latitude : 45.472651,
+    longitude : -122.630036
+  },
+  elliot: {
+    latitude : 45.541184,
+    longitude : -122.668490
+  },
+  farSouthwest: {
+    latitude : 45.456595,
+    longitude : -122.701771
+  },
+  fosterPowell: {
+    latitude : 45.493112,
+    longitude : -122.589493
+  },
+  governmentIsland: {
+    latitude : 45.581233,
+    longitude : -122.527971
   },
   grantPark: {
     latitude : 45.536569,
@@ -38,6 +90,22 @@ export let neighborhoods = {
   haydenIsland: {
     latitude : 45.617700,
     longitude : -122.693409
+  },
+  hayhurst: {
+    latitude : 45.617700,
+    longitude : -122.730811
+  },
+  hillsdale: {
+    latitude : 45.478911,
+    longitude : -122.695771
+  },
+  hosford: {
+    latitude : 45.506227,
+    longitude : -122.651681
+  },
+  homestead: {
+    latitude : 45.496103,
+    longitude : -122.688931
   },
   humboldt: {
     latitude : 45.559996,
@@ -55,13 +123,41 @@ export let neighborhoods = {
     latitude : 45.525716,
     longitude :  -122.642992
   },
+  king: {
+    latitude : 45.557454,
+    longitude :  -122.658531
+  },
   laurelhurst: {
     latitude : 45.528031,
     longitude : -122.625568
   },
+  lents: {
+    latitude : 45.479405,
+    longitude : -122.564743
+  },
   linnton: {
     latitude : 45.599844,
     longitude : -122.786254
+  },
+  lloyd: {
+    latitude : 45.531245,
+    longitude : -122.660420
+  },
+  madisonSouth: {
+    latitude : 45.540744,
+    longitude :  -122.572359
+  },
+  maplewoodAshcreek: {
+    latitude : 45.467031,
+    longitude :  -122.733749
+  },
+  montavilla: {
+    latitude : 45.519039,
+    longitude :  -122.577508
+  },
+  mtScott: {
+    latitude : 45.481077,
+    longitude :  -122.592585
   },
   nobHill: {
     latitude : 45.528571,
@@ -70,6 +166,10 @@ export let neighborhoods = {
   northwest: {
     latitude : 45.533562,
     longitude : -122.701271
+  },
+  overlook: {
+    latitude : 45.558896,
+    longitude : -122.692849
   },
   parkrose: {
     latitude : 45.557190,
@@ -91,6 +191,10 @@ export let neighborhoods = {
     latitude : 45.492284,
     longitude: -122.538667
   },
+  reed: {
+    latitude : 45.484653,
+    longitude : -122.633284
+  },
   richmond: {
     latitude : 45.503553,
     longitude : -122.625354
@@ -98,6 +202,18 @@ export let neighborhoods = {
   roseway: {
     latitude : 45.546728,
     longitude : -122.584155
+  },
+  sellwoodMoreland: {
+    latitude: 45.471207,
+    longitude:-122.652195
+  },
+  southTabor: {
+    latitude: 45.501115,
+    longitude:-122.593655
+  },
+  southwestHills: {
+    latitude: 45.502646,
+    longitude:-122.593655
   },
   stJohns: {
     latitude: 45.592097,
@@ -118,6 +234,10 @@ export let neighborhoods = {
   woodlawn: {
     latitude : 45.570512,
     longitude :  -122.653013
+  },
+  woodstock: {
+    latitude : 45.480355,
+    longitude :  -122.614406
   }
 };
 
