03624e690adff55d8d46c31aecd778871e7ecc0d
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-09-15 14:24:47 -0700

    added more logic to main.js to filter and process all the listings.
    Co-authored-by: Forrest Reiland <freiland@email.arizona.edu>

--- src/main.js
+++ src/main.js
@@ -3,49 +3,76 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import RentalService from './rental-service.js';
-import {UserNeighborhood, neighborhoods} from './neighborhood.js';
+import {neighborhoods} from './neighborhood.js';
 
 function getElements(response){
   if (response) {
     // $("#outputRentLow").text(response.rentRangeLow);
     // $("#outputRentHigh").text(response.rentRangeHigh);
-    $("#outputListing1Address").text(response.listings[0].address);
-    $("#outputListing1Price").text(response.listings[0].price);
-    $("#outputListing1SquareFootage").text(response.listings[0].squareFootage);
+    $("#outputListing1Address").text("Address: " + response.listings[0].address);
+    $("#outputListing1Price").text("Price: " + response.listings[0].price);
+    $("#outputListing1SquareFootage").text("Square Footage: " + response.listings[0].squareFootage);
     $("#outputListing1DaysOnMarket").text("This property has been on the market for " + response.listings[0].daysOld + " days.");
-    $("#outputListing1Bedrooms").text(response.listings[0].bedrooms);
-    $("#outputListing1Bathrooms").text(response.listings[0].bathrooms);
+    $("#outputListing1Bedrooms").text("Bedrooms: " + response.listings[0].bedrooms);
+    $("#outputListing1Bathrooms").text("Bathrooms: " + response.listings[0].bathrooms);
     
 
     $("#outputListing2").text(response.listings[1]);
     $("#outputListing3").text(response.listings[2]);
     $("#outputListing4").text(response.listings[3]);
     $("#outputListing5").text(response.listings[4]);
-    console.log(response);
+    console.log(response.listings);
     
-    // Display Listing //
+    // Display Listing TEST TEST//
     // lisitng array from API call will be a variable number of elements from 1-10 //
-    response.listings.forEach(property) {
+    
+    
+    // function writeListing(property) {
+    //   let html = '';
+    //   html += `<div class="carousel-item active" >`;
+    //   html += `<div class='card search-card col-6'><div class='card-header'>${property[0].address}</div>`;
+    //   html += `<div class='card-body'> Monthly Rent: ${property[0].price}</div>`;
+    //   html += `<p>${property[0].bedrooms}</p>`;
+    //   html += `</div></div>`;
+    //   for(let i = 1; i <= response.listings.length; i++) {
+    //     html += `<div class="carousel-item " >`;
+    //     html += `<div class='card search-card col-6'><div class='card-header'>${property[i].address}</div>`;
+    //     html += `<div class='card-body'> Monthly Rent: ${property[i].price} </div>`;
+    //     html += `<p>${property[i].bedrooms}</p>`;
+    //     html += `</div></div>`;
+    //   }
+    //   //console.log(html);
+    //   $('.carousel-inner').html(html);
       
-      $("#outputListing1Address").text(property.address);
+    // } 
+    
+    
+    
+    
+    
+    response.listings.forEach(function(property) {
+      console.log(property);
+
+      console.log(property.address);
+      //$("#outputListing1Address").text(property.address);
       
-      $("#outputListing1Price").text(property.price);
+      //$("#outputListing1Price").text(property.price);
       
-      $("#outputListing1SquareFootage").text(property.squareFootage);
+      //$("#outputListing1SquareFootage").text(property.squareFootage);
       
-      $("#outputListing1DaysOnMarket").text("This property has been on the market for " + response.listings[0].daysOld + " days.");
+      //$("#outputListing1DaysOnMarket").text("This property has been on the market for " + response.listings[0].daysOld + " days.");
       
-      $("#outputListing1Bedrooms").text(response.listings[0].bedrooms);
+      //$("#outputListing1Bedrooms").text(response.listings[0].bedrooms);
       
-      $("#outputListing1Bathrooms").text(response.listings[0].bathrooms);
-    }
+      //$("#outputListing1Bathrooms").text(response.listings[0].bathrooms);
+    });
 
     
     
     // use a carousel or other bootstrap feature to allow user to look through lisitngs in neighborhood //
 
     // Test output
-    $("#result").text(response)
+    $("#result").text(response);
     console.log(response);
   }
 }
@@ -54,35 +81,32 @@ function getElements(response){
 $(document).ready(function () {
   $("#executeButton").click(function () {
     // test input
-    let neighborhoodInput = "stJohns";
+    let neighborhoodInput = "boise";
 
     // let neighborhoodInput = $("#neighborhood");
     // findNeighborhoodCoordinates(neighborhoodInput);
     let input = 1200;
     let squareFootageInput = "&squareFootage="+input;  
-    // let input = 1200;
-    // let squareFootageInput = "&squareFootage="+input;
-    console.log(squareFootageInput);
+  
+    // let squareFootageInput = $("#squareFootage");
     let bathroomInput = 3;
     let propertyTypeInput = "apartment";
     let bedroomInput = 4;
 
+    // writeListings(reponse.listings);
+    
     // let squareFootageInput = $("#squareFootage");
     // let bathroomInput = $("#bathrooms");
     // let propertyTypeInput = $("#property");
     // let bedroomInput = $("#bedroom");
+    
     const latitudeInput = neighborhoods[neighborhoodInput].latitude;
     const longitudeInput = neighborhoods[neighborhoodInput].longitude;
 
     // FORREST TEST CODE //
 
-    const inputNeighborhood = new UserNeighborhood (neighborhoodInput);
     
-    console.log(inputNeighborhood);
-
     console.log(neighborhoods[neighborhoodInput].latitude);
-    
-    let latLong = inputNeighborhood.getCoordinates();
     
     
     
--- src/neighborhood.js
+++ src/neighborhood.js
@@ -1,128 +1,111 @@
-export class UserNeighborhood {
-  constructor(neighborhood) {
-    this.neighborhood = neighborhood;
+export let neighborhoods = {
+  alameda: {
+    latitude : 45.546308,
+    longitude : -122.634902
+  },
+  alberta: {
+    latitude : 45.552319,
+    longitude :  -122.654043
+  },
+  argay: {
+    latitude : 45.552285,
+    longitude :  -122.520164
+  },
+  belmont: {
+    latitude : 45.514741,
+    longitude : -122.616256
+  },
+  boise: {
+    latitude : 45.549749,
+    longitude : -122.672539
+  },
+  bridgeton: {
+    latitude : 45.602286,
+    longitude : -122.668072
+  },
+  buckman: {
+    latitude : 45.518169,
+    longitude :  -122.648421
+  },
+  grantPark: {
+    latitude : 45.536569,
+    longitude : -122.628100
+  },
+  haydenIsland: {
+    latitude : 45.617700,
+    longitude : -122.693409
+  },
+  humboldt: {
+    latitude : 45.559996,
+    longitude : -122.663913
+  },
+  irvington: {
+    latitude : 45.543423,
+    longitude : -122.653313
+  },
+  kentonPark: {
+    latitude : 45.581927,
+    longitude : -122.694039
+  },
+  kerns: {
+    latitude : 45.525716,
+    longitude :  -122.642992
+  },
+  laurelhurst: {
+    latitude : 45.528031,
+    longitude : -122.625568
+  },
+  linnton: {
+    latitude : 45.599844,
+    longitude : -122.786254
+  },
+  nobHill: {
+    latitude : 45.528571,
+    longitude :  -122.693139
+  },
+  northwest: {
+    latitude : 45.533562,
+    longitude : -122.701271
+  },
+  parkrose: {
+    latitude : 45.557190,
+    longitude : -122.551075
+  },
+  piedmont: {
+    latitude : 45.567868,
+    longitude : -122.666359
+  },
+  portsmouth: {
+    latitude : 45.589405,
+    longitude: -122.719251
+  },
+  richmond: {
+    latitude : 45.503553,
+    longitude : -122.625354
+  },
+  roseway: {
+    latitude : 45.546728,
+    longitude : -122.584155
+  },
+  stJohns: {
+    latitude: 45.592097,
+    longitude:-122.747639
+  },
+  sunderland: {
+    latitude: 45.582485,
+    longitude:-122.637551
+  },
+  universityPark: {
+    latitude : 45.581086,
+    longitude: -122.730066
+  },
+  woodlawn: {
+    latitude : 45.570512,
+    longitude :  -122.653013
   }
-
-  getCoordinates () {
-
-    let latitude = this.latitude;
-    let longitude = this.longitude;
-    return latitude;
-  }
-}
+};
 
 
-let universityPark = {
-  latitude : 45.581086,
-  longitude: -122.730066
-}
 
-let kentonPark = {
-  latitude : 45.581927,
-  longitude : -122.694039
-}
-
-let piedmont = {
-  latitude : 45.567868,
-  longitude : -122.666359
-}
-
-let humboldt = {
-  latitude : 45.559996,
-  longitude : -122.663913
-}
-
-let boise = {
-  latitude : 45.549749,
-  longitude : -122.672539
-}
-
-let woodlawn = {
-  latitude : 45.570512,
-  longitude :  -122.653013
-}
-
-export let neighborhoods = {alberta: {
-  latitude : 45.552319,
-  longitude :  -122.654043
-},
-stJohns: {
-  latitude: 45.592097,
-  longitude:-122.747639
-},
-portsmouth: {
-  latitude : 45.589405,
-  longitude: -122.719251
-},
-universityPark: {
-  latitude : 45.581086,
-  longitude: -122.730066
-}
-}
-
-let irvington = {
-  latitude : 45.543423,
-  longitude : -122.653313
-}
-
-
-let alameda = {
-  latitude : 45.546308,
-  longitude : -122.634902
-}
-
-let grantPark = {
-  latitude : 45.536569,
-  longitude : -122.628100
-  
-}
-
-let laurelhurst = {
-  latitude : 45.528031,
-  longitude : -122.625568
-  
-}
-
-let kerns = {
-  latitude : 45.525716,
-  longitude :  -122.642992
-  
-}
-
-let buckman = {
-  latitude : 45.518169,
-  longitude :  -122.648421
-  
-}
-
-let belmont = {
-  latitude : 45.514741,
-  longitude : -122.616256
-  
-}
-
-let richmond = {
-  latitude : 45.503553,
-  longitude : -122.625354
-  
-}
-
-let roseway = {
-  latitude : 45.546728,
-  longitude : -122.584155
-  
-}
-
-let northwest = {
-  latitude : 45.533562,
-  longitude : -122.701271
-  
-}
-
-let nobHill = {
-  latitude : 45.528571,
-  longitude :  -122.693139
-  
-}
 
 
